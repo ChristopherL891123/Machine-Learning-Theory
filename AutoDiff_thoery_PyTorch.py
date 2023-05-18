24b2c5a0ea622aab19b1f5c92fc58453baafbe3c
Lama, Christopher <77355477+ChristopherL891123@users.noreply.github.com>
2023-05-18 12:10:50 -0500

    Update AutoDiff_thoery_PyTorch.py

--- AutoDiff_thoery_PyTorch.py
+++ AutoDiff_thoery_PyTorch.py
@@ -9,24 +9,25 @@ import matplotlib.pyplot as plt
 # Steps:
 #       + Forward Propagation: PyTorch builds a computation graph and saves the operations on parameters.
 
-#       + Calculate Loss: Mean Square Error, in this sample script.
+#       + Calculate Loss: It is done to understand the difference between model output and real data. Calculated through Mean Square Error, in this sample script.
 
 #       + Backward Propagation:
 #          - PyTorch iterates reversely through the computational graph
 #           and calculates the derivative of y with respect to each parameter, i.e Automatic Differentiation.
 
 #          - Calculate gradients of the loss function with respect to the model parameters
-#          - When Loss is calculated each Epoch (iteration), PyTorch uses these values to calculate derivative of loss
+#          -  Starts differentiating from the last node(operation) of the graph with respect to the output and works in this way backwards to the first node.
+#          - When Loss is calculated in each Epoch (iteration), PyTorch uses these values to calculate derivative of loss
 #            with respect to the parameter using the chain rule.
 
 #       + Optimize the parameters: Through Gradient Descent, in this sample script.
 #           - Gradients are used to calculate the magnitude of the slope of the ascent or descent of a function.
 #           - Therefore Gradient Descent uses the descent (the minimum) of the Loss MSE function.
 #           - Gradient Descent is used to lower the Loss score (Cost C in this sample script)
-#           - Starts differentiating from the last node(operation) of the graph and
+#           - 
 #           - Gradient Descent changes values of parameters to bring down the Loss as much as possible in each iteration.
 #                This would result in increasing/decreasing a parameter based on the value of the other parameters. Also, if a parameter has a large negative or positive value,
-#                   then a corresponding increase or decrease would affect the value of the Loss more than an increase/decrease in other parameters.
+#                   then a corresponding increase or decrease would affect the value of the Loss more than an increase/decrease in other parameters that do not have large values.
 #               In this way, the model fits the training data in the best way possible to the model.
 #           - Updating Parameters:
 #               # θ is the parameter , a is the learning rate(magnitude of change in each parameter optimization iteration),
@@ -47,18 +48,18 @@ def MSE(y,y_hat):
 
 y = t.tensor([1.86, 1.31, 0.62, 0.33, 0.09, -0.67, -1.23, -1.37])
 
-# set up the m parameter
-
-m = t.tensor([0.9]).requires_grad_()
-
-# set up x input for the model (graph)
-x_input = t.tensor([i for i in range(8)])
-
 # set up the b parameter and keep track of how the parameter influences the result of the model
 # PyTorch keeps track of all operations performed on b during the forward pass(forward propagation) phase
 # Therefore PyTorch calculates derivatives of the loss with respect to b in the backward pass
 # Also applying the method will cause the same effect on any other tensor that results from b.
 b = t.tensor([0.1]).requires_grad_()
+
+# set up the m parameter
+
+m = t.tensor([0.9]).requires_grad_()
+
+# set up x input for the model(graph)
+x_input = t.tensor([i for i in range(8)])
 
 # Forward Propagation
 y_hat = linear_regression(m,x_input,b)
